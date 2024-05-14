42d0a96ecd979ca32eccf9d14cb7869d676c98a5
BiTech5 <bikramkhatri050@gmail.com>
2024-05-14 10:25:09 +0545

    Restaurant-Frontend

---
+++ script.js
@@ -0,0 +1,12 @@
+let menulist=document.getElementById('menulist')
+menulist.style.maxHeight="0px";
+function tooglelist(){
+    if(menulist.style.maxHeight=="0px"){
+menulist.style.maxHeight="400px";
+
+    }
+    else{
+menulist.style.maxHeight="0px";
+
+    }
+}
